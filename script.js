b26f8e2a0cce6df0bf21b09801484382bb16b693
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2023-11-30 08:10:02 +0100

    test

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// Define the function to adjust the height
+function scrollHeight() {
+    var content = document.querySelector('#parchment');
+    var container = document.querySelector('#contain');
+    // SVG feTurbulence can modify all other elements, for this reason "parchment" is in another <div> and in absolute position.
+    // so for a better effect, absolute height is defined by its content.
+    content.style.height = container.offsetHeight + 'px';
+}
+
+// Call the function after the page is fully loaded
+window.onload = function () {
+    scrollHeight();
+};
 
 // Redraw when viewport is modified
 window.addEventListener('resize', function (event) {
-    ScrollHeight();
+    scrollHeight();
 });
-
-
-function ScrollHeight() {
-    var content = document.querySelector('#parchment');
-    var container = document.querySelector('#contain');
-    var img = document.querySelector(".img")
-    // SVG feTurbulence can modify all others elements, for this reason "parchment" is in another <div> and in absolute position.
-    // so for a better effect, absolute height is defined by his content.
-    content.style.height = container.offsetHeight + 'px';
-}
-// First call to define "parchment" height
-document.onload = ScrollHeight();
