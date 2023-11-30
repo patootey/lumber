4f8fe24eae85df0bd7822e9cfed1efe9071190b2
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2023-11-30 08:12:01 +0100

    w

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 function scrollHeight() {
     var content = document.querySelector('#parchment');
     var container = document.querySelector('#contain');
+    var img = document.querySelectorAll('.image img');
+
     // SVG feTurbulence can modify all other elements, for this reason "parchment" is in another <div> and in absolute position.
     // so for a better effect, absolute height is defined by its content.
     content.style.height = container.offsetHeight + 'px';
-}
 
+    // Loop through img elements and add styles
+    img.forEach(function (image) {
+        image.style.objectFit = 'cover';
+        image.style.width = '100%';
+        image.style.height = '100%';
+    });
+}
 // Call the function after the page is fully loaded
 window.onload = function () {
     scrollHeight();
